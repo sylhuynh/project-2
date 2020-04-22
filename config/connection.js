d3b82d33678f15d2b9860f90f581c1769b4f74b7
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-04-22 14:01:20 -0700

    created connection.js file

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "wanderful_db"
+  });
+}
+
+connection.connect(err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
