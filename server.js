885fb874697a3689f6fe8aeee606892665fef3dd
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-22 13:10:31 -0700

    Add the .env file to store keys and update the config for production to use the jawsdb env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
+require("dotenv").config();
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
@@ -15,7 +16,11 @@ app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({
+    secret: process.env.SERVER_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
