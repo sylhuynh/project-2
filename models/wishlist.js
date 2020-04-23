5a6fec52e31265103a0167856091290c5558f7f9
mcerrillo1 <mcerrillo1@yahoo.com>
2020-04-22 17:25:28 -0700

    added visiteds and wishlists tables

---
+++ models/wishlist.js
@@ -0,0 +1,41 @@
+module.exports = function(sequelize, DataTypes) {
+  const Wishlist = sequelize.define(
+    "Wishlist",
+    {
+      cityName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      countryName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      wishlistStatus: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+      visitedStatus: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+    }
+    // {
+    //   freezeTableName: true
+    // }
+  );
+
+  //   Wishlist.associate = function(models) {
+  //     Wishlist.belongsTo(models.Visited, {
+  //       foreignKey: {
+  //         allowNull: false
+  //       }
+  //     });
+  //   };
+  return Wishlist;
+};
