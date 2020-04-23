451f984053f54c2af18a2153d0d663b461d020b6
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-04-23 15:03:18 -0700

    added html route for wishlist/visited/destination page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/views/signup.html"));
   });
 
   app.get("/login", function(req, res) {
@@ -18,20 +18,40 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/views/login.html"));
+  });
+
+  app.get("/wishlist", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/wishlist");
+    }
+    res.sendFile(path.join(__dirname, "../public/views/wishlist.html"));
+  });
+
+  app.get("/visited", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/visited");
+    }
+    res.sendFile(path.join(__dirname, "../public/views/visited.html"));
+  });
+
+  app.get("/destination", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/views/destination.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/views/signup.html"));
   });
 
   app.get("/wishlist", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/views/signup.html"));
   });
 
   app.get("/visited", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/views/signup.html"));
   });
 };
