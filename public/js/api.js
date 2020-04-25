001728ac113f64426a74dce16163a809ac864f5f
mcerrillo1 <mcerrillo1@yahoo.com>
2020-04-24 18:18:56 -0700

    created dynamic append for visited card

--- public/js/api.js
+++ public/js/api.js
@@ -108,31 +108,3 @@ function addWishlist(destination) {
 }
 
 $("#search").hide();
-
-
-`<div class="uk-container uk-margin-medium uk-margin-large-bottom">
-<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
-  <div class="uk-card-media-left uk-cover-container">
-    <img src="${url}" alt="" uk-cover>
-    <canvas width="600" height="400"></canvas>
-  </div>
-  <div>
-    <div class="uk-card-body">
-      <h3 class="uk-card-title">${destination}</h3>
-      <br>
-      <div class="uk-card-footer">
-        <ul class="uk-iconnav">
-          <a class="uk-button uk-button-small uk-link-reset add-visited">
-            <span class="uk-icon uk-margin-small-right" uk-icon="icon: location" id="san-diego-visited"></span>
-            Add visited
-          </a>
-          <a class="uk-button uk-button-small uk-link-reset add=wishlist">
-            <span class="uk-icon uk-margin-small-right" uk-icon="icon: heart" id="san-diego-wishlist"></span>
-            Add wishlist
-          </a>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
-</div>`
